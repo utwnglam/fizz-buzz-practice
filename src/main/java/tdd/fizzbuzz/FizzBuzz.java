a68ef64d311f13f7029502275c3a4cbc3f9c17db
CHEUNJO2 <cheunjo2@oocl.com>
2021-12-02 14:02:39 +0800

    [FizzBuzzPractice] [joanne] feat: TestCase 1, 2

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,7 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+  public String countOff(int count_off_order) {
+    return null;
+  }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,24 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FizzBuzzTest {
+  @Test
+  public void should_count_off_number_multiple_of_3_return_Fizz() {
+    int count_off_order = 3;
+
+    FizzBuzz fizzBuzzGame = new FizzBuzz();
+    String countOffString = fizzBuzzGame.countOff(count_off_order);
+    assertThat(countOffString).isEqualTo("Fizz");
+  }
+
+  @Test
+  public void should_count_off_number_multiple_of_5_return_Buzz() {
+    int count_off_order = 5;
+
+    FizzBuzz fizzBuzzGame = new FizzBuzz();
+    String countOffString = fizzBuzzGame.countOff(count_off_order);
+    assertThat(countOffString).isEqualTo("Buzz");
+  }
 }
